4088b94a624ad6533f85933b4d4ecb01240128ea
ivanmiguelmolinero <i.miguel.molinero@gmail.com>
2022-05-16 13:57:44 +0200

    Iniciamos repo

---
+++ src/test/java/practica/vermont/store/customer/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package practica.vermont.store.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
